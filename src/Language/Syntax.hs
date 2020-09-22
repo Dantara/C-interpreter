7f7a78f167afc3f767c97a6f4b8322965a9d2b66
Dantara <supercreed2@gmail.com>
2020-09-22 17:53:24 +0300

    Token types

---
+++ src/Language/Syntax.hs
@@ -0,0 +1,73 @@
+module Language.Syntax where
+
+data TokenType
+  = TokenTypeInt
+  | TokenTypeFloat
+  | TokenTypeDouble
+  | TokenTypeBool
+  deriving (Eq, Show)
+
+data TokenKeyword
+  -- Conditions
+  = TokenIf
+  | TokenElse
+  -- Loops
+  | TokenFor
+  | TokenWhile
+  -- Return
+  | TokenReturn
+  deriving (Eq, Show)
+
+data TokenDelimiter
+  = TokenComma
+  | TokenSemicolon
+  -- Brackets
+  | TokenOpenRoundBracket
+  | TokenCloseRoundBracket
+  | TokenOpenCurlyBracket
+  | TokenCloseCurlyBracket
+  deriving (Eq, Show)
+
+data TokenArithmeticOperator
+  = TokenPlus
+  | TokenMinus
+  | TokenMultiply
+  | TokenDivide
+  deriving (Eq, Show)
+
+data TokenBoolOperator
+  = TokenAnd
+  | TokenOr
+  | TokenNot
+  | TokenXor
+  deriving (Eq, Show)
+
+data TokenOperator
+  = TokenArithmeticOperator
+  | TokenBoolOperator
+  | TokenAssignment
+  deriving (Eq, Show)
+
+data TokenValue
+  = TokenString String
+  | TokenInt Int
+  | TokenFloat Float
+  | TokenDouble Double
+  | TokenBool Bool
+  deriving (Eq, Show)
+
+data Token
+  -- Keyword
+  = TokenKeyword
+  -- Delimiter
+  | TokenDelimiter
+  -- Operator
+  | TokenOperator
+  -- Type
+  | TokenTypeDeclaration TokenType
+  -- Identifer
+  | TokenIndentifier String
+  -- Value
+  | TokenValue
+  -- End of the file
+  | TokenEOF
